40a7ee570d6f7a86e56c33e4aa095ab9ee134fe8
iLYuSha <ilyusha71@gmail.com>
2016-08-13 14:27:24 +0800

    1st commit

---
+++ Magic_Altar/Magic_Altar.ino
@@ -0,0 +1,76 @@
+/******************************************
+  Title: Magic Altar
+  Studio: iLYuSha Wakaka & 0escape
+  Author: By iLYuSha Wakaka KocmocA
+  2015/10/30
+*******************************************/
+/* Input */
+const int holyGrail = 2;
+const int dragonFang = 3;
+const int roundTable = 4;
+const int crown = 5;
+const int heartDestruction = 6;
+const int heartRedemption = 7;
+/* Output */
+const int activeAltar = 8; //uvLight,caliburn
+const int emLockMagic = 9; // Magician Room
+const int magic = 11; // Animation 1st
+const int destruction = 12; // Animation 2nd
+const int redemption = 13; // Animation 3rd
+/* Function */
+int holyGrailValue = 0;
+int dragonFangValue = 0;
+int roundTableValue = 0;
+int crownValue = 0;
+int heartDestructionValue = 0;
+int heartRedemptionValue = 0;
+boolean enableMagicAltar = false;
+
+void setup() {
+  Serial.begin(9600);        // Initialize serial communications with the PC
+  pinMode(holyGrail,INPUT);
+  pinMode(dragonFang,INPUT);
+  pinMode(roundTable,INPUT);
+  pinMode(crown,INPUT);
+  pinMode(heartDestruction,INPUT);
+  pinMode(heartRedemption,INPUT);  
+  pinMode(activeAltar,OUTPUT);
+  pinMode(emLockMagic,OUTPUT);
+  pinMode(magic,OUTPUT);
+  pinMode(destruction,OUTPUT);
+  pinMode(redemption,OUTPUT);
+  digitalWrite(activeAltar, LOW);
+  digitalWrite(emLockMagic, LOW);
+  digitalWrite(magic, LOW);
+  digitalWrite(destruction, LOW);
+  digitalWrite(redemption, LOW);
+  delay(1000);
+  Serial.println("Magic Altar 2015/10/30 iLYuSha");
+}
+
+void loop() {
+  holyGrailValue = digitalRead(holyGrail);
+  dragonFangValue = digitalRead(dragonFang);
+  roundTableValue = digitalRead(roundTable);
+  crownValue = digitalRead(crown);
+  heartDestructionValue = digitalRead(heartDestruction);
+  heartRedemptionValue = digitalRead(heartRedemption);
+  
+  if(holyGrailValue == 1 && dragonFangValue == 1 && roundTableValue == 1 && crownValue == 1)
+  {
+    digitalWrite(activeAltar, HIGH);
+    digitalWrite(magic, HIGH);
+    enableMagicAltar = true;
+  }
+  
+  if(enableMagicAltar && heartDestructionValue == 1)
+  {
+    digitalWrite(destruction, HIGH);
+  }
+
+  if(enableMagicAltar && heartRedemptionValue == 1)
+  {
+    digitalWrite(emLockMagic, HIGH);
+    digitalWrite(redemption, HIGH);
+  }
+}
